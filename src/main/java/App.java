537ac10e5c4cca17d4f6cfa9d4d444b7e10dfac0
AndreasSjo <andreas.sjoden@hotmail.com>
2022-01-05 13:43:12 +0100

    testar

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("asdasd");
-        Scanner scanner = new Scanner(System.console().readLine());
+        System.out.println("Testar om git funkar");
     }
 }
