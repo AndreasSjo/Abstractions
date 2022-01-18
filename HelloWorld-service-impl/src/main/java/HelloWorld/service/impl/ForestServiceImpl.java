2ae1310a0cfcb6a5af243a36b03f3951f871e0f4
Ograeset <tedros963@gmail.com>
2022-01-18 13:33:45 +0100

    remove duplicates

--- HelloWorld-service-impl/src/main/java/HelloWorld/service/impl/ForestServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package HelloWorld.service.impl;
-
-import HelloWorld.domain.Owl;
-
-public class ForestServiceImpl {
-    Owl owl;
-
-}
--- HelloWorld-service/src/main/java/HelloWorld/service/ForestService.java
+++
@@ -1,7 +0,0 @@
-package HelloWorld.service;
-
-import HelloWorld.domain.Owl;
-
-public interface ForestService {
-    Owl owl = new Owl();
-}
