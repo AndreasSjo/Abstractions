6959f8cb5e88d089ff3dc7aa8f6b25d4da17ab57
AndreasSjo <andreas.sjoden@hotmail.com>
2022-01-20 14:00:21 +0100

    add testService

--- HelloWorld-test/HelloWorld-test-unit/src/test/java/HelloWorld/test/unit/TestService.java
+++ HelloWorld-test/HelloWorld-test-unit/src/test/java/HelloWorld/test/unit/TestService.java
@@ -1,14 +1,26 @@
 package HelloWorld.test.unit;
 
+import HelloWorld.domain.Owl;
+import HelloWorld.service.ForestService;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestService {
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:HelloWorld-service.xml");
 
+    private ForestService forestService;
+
     @Before
     public void before(){
-        applicationContext.getBean("forestService");
+        forestService = (ForestService)applicationContext.getBean("forestService");
+    }
+
+    @Test
+    public void testForestServiceDependencyInjection(){
+        Owl owl = forestService.createOwl(95,"male","olof");
+        Assert.assertNotNull(owl);
     }
 }
